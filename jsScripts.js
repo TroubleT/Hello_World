df8c936471cfd6f7ed4b984b7e714c1306ce387a
Hryhoriy Starykov <starykovhryhoriy@gmail.com>
2018-07-17 22:11:34 +0000

    random added

--- jsScripts.js
+++ jsScripts.js
@@ -1,6 +1,6 @@
 function changeColor() {
     let textHello = document.getElementById("text");
-    textHello.style.color(random(), random(), random());
+    textHello.style.color="random(), random(), random()";
 }
 function random(){
     Math.floor(Math.random() * 257);
